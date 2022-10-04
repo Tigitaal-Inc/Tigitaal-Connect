e819394be021672fdc2b3a1b532f0433644846a5
Tigitaal <82046482+Tigitaal@users.noreply.github.com>
2022-10-04 20:46:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.4' 
+VERSION = '0.0.5' 
 DESCRIPTION = 'Tigitaal\'s Official Python Package to connect with TigitaalAPI'
 
 # Setting up
 setup(
         name="tigitaalconnect", 
         version=VERSION,
-        author="Ninjagor",
-        author_email="ninjagor.spoon@gmail.com",
+        author="Ninjagor, Tigitaal",
+        author_email="ninjagor.spoon@gmail.com, info@tychovandenheuvel.nl",
         description=DESCRIPTION,
         long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
         packages=find_packages(),
