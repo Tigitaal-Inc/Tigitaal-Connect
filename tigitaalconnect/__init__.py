f11d22c4359270ca4447e6f66201b8bf2147e3c4
Tigitaal <82046482+Tigitaal@users.noreply.github.com>
2022-10-04 20:38:45 +0200

    User class
    added the user class + some minor changes

--- tigitaalconnect/__init__.py
+++ tigitaalconnect/__init__.py
@@ -1,22 +1,33 @@
 import requests
 
-def reload(token):
-    response = requests.get(f'https://api.tigitaal.nl/user/reload/{token}')
-    return response.json()
+class user():
+    
+  def __init__(self, username, password):
+      self.username = username
+      self.password = password
+      self.token = requests.post(f'https://api.tigitaal.nl/auth/login/{username}/{password}').json()["data"]
+    
+  def reload():
+      response = requests.get(f'https://api.tigitaal.nl/user/reload/{self.token}')
+      return response.json()
 
-def reloadadvanced(username, password):
-    response = requests.get(f'https://api.tigitaal.nl/user/reloadadvanced/{username}/{password}')
-    return response.json()
+  def reloadadvanced():
+      response = requests.get(f'https://api.tigitaal.nl/user/reloadadvanced/{self.username}/{self.password}')
+      return response.json()
 
-# Buggy/doesnt work
-def pfp(token, pfp):
-    response = requests.post(f'https://api.tigitaal.nl/user/reload/{token}/{pfp}')
-    return response.json()
+  # Buggy/doesnt work
+  def pfp(pfp):
+      response = requests.post(f'https://api.tigitaal.nl/user/reload/{self.token}/{pfp}')
+      return response.json()
 
-def nickname(username, password, nickname):
-    response = requests.post(f'https://api.tigitaal.nl/user/nickname/{username}/{password}/{nickname}')
-    return response.json()
+  def nickname(nickname):
+      response = requests.post(f'https://api.tigitaal.nl/user/nickname/{self.username}/{self.password}/{nickname}')
+      return response.json()
 
-def mail(username, password, email):
-    response = requests.post(f'https://api.tigitaal.nl/user/mail/{username}/{password}/{email}')
-    return response.json()
+  def mail(email):
+      response = requests.post(f'https://api.tigitaal.nl/user/mail/{self.username}/{self.password}/{email}')
+      return response.json()
+  
+  def aboutme(aboutme):
+      response = requests.post(f'https://api.tigitaal.nl/user/aboutme/{self.username}/{self.password}/{aboutme}')
+      return response.json()
