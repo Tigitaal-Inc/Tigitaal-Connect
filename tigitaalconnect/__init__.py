6828877ca9f035267710d65db18341fd74c92776
Ninjagor <88942472+Ninjagor@users.noreply.github.com>
2022-10-05 16:17:16 -0400

    v 0.0.5

--- tigitaalconnect/__init__.py
+++ tigitaalconnect/__init__.py
@@ -1,33 +1,33 @@
 import requests
 
-class user():
+class UserConnect():
     
   def __init__(self, username, password):
       self.username = username
       self.password = password
       self.token = requests.post(f'https://api.tigitaal.nl/auth/login/{username}/{password}').json()["data"]
     
-  def reload():
+  def reload(self):
       response = requests.get(f'https://api.tigitaal.nl/user/reload/{self.token}')
       return response.json()
 
-  def reloadadvanced():
+  def reloadadvanced(self):
       response = requests.get(f'https://api.tigitaal.nl/user/reloadadvanced/{self.username}/{self.password}')
       return response.json()
 
   # Buggy/doesnt work
-  def pfp(pfp):
+  def pfp(self, pfp):
       response = requests.post(f'https://api.tigitaal.nl/user/reload/{self.token}/{pfp}')
       return response.json()
 
-  def nickname(nickname):
+  def nickname(self, nickname):
       response = requests.post(f'https://api.tigitaal.nl/user/nickname/{self.username}/{self.password}/{nickname}')
       return response.json()
 
-  def mail(email):
+  def mail(self, email):
       response = requests.post(f'https://api.tigitaal.nl/user/mail/{self.username}/{self.password}/{email}')
       return response.json()
   
-  def aboutme(aboutme):
-      response = requests.post(f'https://api.tigitaal.nl/user/aboutme/{self.username}/{self.password}/{aboutme}')
-      return response.json()
+#   def aboutme(self, aboutme):
+#       response = requests.post(f'https://api.tigitaal.nl/user/aboutme/{self.username}/{self.password}/{aboutme}')
+#       return response.json()
