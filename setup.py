9ad6690b9cc412fcefc592908a4de94a2a20aedf
Ninjagor <88942472+Ninjagor@users.noreply.github.com>
2022-10-08 12:06:26 -0400

    v0.0.6

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from setuptools import setup, find_packages
+import codecs
+import os
 
-VERSION = '0.0.5' 
+here = os.path.abspath(os.path.dirname(__file__))
+with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as fh:
+    long_desciption = '\n' + fh.read() + '\n\n' + open('CHANGELOG.txt').read()
+
+VERSION = '0.0.6' 
 DESCRIPTION = 'Tigitaal\'s Official Python Package to connect with TigitaalAPI'
 
 # Setting up
 setup(
         name="tigitaalconnect", 
         version=VERSION,
-        author="Ninjagor, Tigitaal",
-        author_email="ninjagor.spoon@gmail.com, info@tychovandenheuvel.nl",
+        author="Ninjagor",
+        author_email="ninjagor.spoon@gmail.com",
         description=DESCRIPTION,
-        long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+        long_description_content_type="text/markdown",
+        long_description=long_desciption,
         packages=find_packages(),
         install_requires=[
             'requests'
@@ -19,7 +26,7 @@ setup(
         keywords=['python', 'first package'],
         classifiers= [
             "Development Status :: 5 - Production/Stable",
-            "Intended Audience :: Education",
+            "Intended Audience :: Developers",
             "Programming Language :: Python :: 3",
             "Operating System :: MacOS :: MacOS X",
             "Operating System :: Microsoft :: Windows",
